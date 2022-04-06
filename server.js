44f2b233f501abeffb8d16dd2e04dd62bf12f417
alexjosesilva <alexjosesilvati@gmail.com>
2022-04-06 13:28:03 -0300

    Conf: apiUrl

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(__dirname + '/dist/consumir-api'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/consumir-api/index.html'));});
+    res.sendFile(path.join(__dirname+'/dist/consumir-api/index.html'));
+});
+
 app.listen(process.env.PORT || 8080); 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL:"http://api-rest-nodjs.herokuapp.com/api"
+  apiURL:"https://api-rest-nodjs.herokuapp.com/api"
 };
