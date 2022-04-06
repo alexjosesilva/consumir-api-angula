2ecb6a5d492d9effdaee7be80297b08d9dd7bd58
alexjosesilva <alexjosesilvati@gmail.com>
2022-04-06 12:35:37 -0300

    DEV | conf env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiURL2:"http://localhost:3000/alunos/",
-  apiURL1:"http://localhost:9090/api",
   apiURL:"http://api-rest-nodjs.herokuapp.com/api"
 };
