311d45e7894f0580a712236a9bfb7ec5acbf4795
alexjosesilva <alexjosesilvati@gmail.com>
2022-04-06 11:56:18 -0300

    Conf |  apiUrl e package

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: true,
-  apiURL:"http://localhost:3000/alunos/"
+  apiURL2:"http://localhost:3000/alunos/",
+  apiURL1:"http://localhost:9090/api",
+  apiURL:"http://api-rest-nodjs.herokuapp.com/api"
+
 };
 
 /*
